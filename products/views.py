23c2147a090aa3f32ec9777b038b3ea3b8835f1a
Jasmeen Saprah <j.sapraa@gmail.com>
2025-02-06 21:48:53 +0000

    Migration vs code

--- products/views.py
+++ products/views.py
@@ -7,4 +7,10 @@ def all_products(request):
 
     """ A view to show all products, including sorting and search queries """
 
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
     return render(request, "products/products.html", context)
